642d771678099bf06874090f5e075dd15120fa5e
AmteurPotion <1k0j2i4@gmail.com>
2023-03-14 04:30:31 +0900

    직렬화되는 키값 구조체

---
+++ Assets/Scripts/Utils/Serialization/SerializablePair.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FabricWars.Utils.Serialization
+{
+    [Serializable]
+    public struct SerializablePair <K, V>
+    {
+        public K key;
+        public V value;
+
+        public SerializablePair(K key, V value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public void Deconstruct(out K key, out V value)
+        {
+            key = this.key;
+            value = this.value;
+        }
+    }
+}

2ca195a8ae234c79acfac1d9cdd02e3599d91c24
AmteurPotion <1k0j2i4@gmail.com>
2023-03-14 03:00:46 +0900

    마스킹 셰이더 컨피그

---
+++ Assets/Scripts/Graphics/MaskingShader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace FabricWars.Graphics
+{
+    [CreateAssetMenu(fileName = "new Mask Config", menuName = "Shader/Mask Config", order = 0)]
+    public class MaskingShader : ScriptableObject
+    {
+        public Color maskColor;
+        public Texture texture;
+    }
+}

d4efaf2a77a8fb73f8d731536fcfbda33b84794b
AmteurPotion <1k0j2i4@gmail.com>
2023-03-14 17:36:45 +0900

    Type 확장메서드

---
+++ Assets/Scripts/Utils/Overrides/TypeOverride.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace FabricWars.Utils.Overrides
+{
+    public static class TypeOverride
+    {
+        public static bool ImplicitlyConvertsTo(this Type type, Type destinationType)
+        {
+            if (type == destinationType)
+                return true;
+
+
+            return (from method in type.GetMethods(BindingFlags.Static |
+                                                   BindingFlags.Public)
+                    where method.Name == "op_Implicit" &&
+                          method.ReturnType == destinationType
+                    select method
+                ).Any();
+        }
+        
+        public static bool ExplicitlyConvertsTo(this Type type, Type destinationType)
+        {
+            if (type == destinationType)
+                return true;
+
+
+            return (from method in type.GetMethods(BindingFlags.Static |
+                                                   BindingFlags.Public)
+                    where method.Name == "op_Explicit" &&
+                          method.ReturnType == destinationType
+                    select method
+                ).Any();
+        }
+    }
+}

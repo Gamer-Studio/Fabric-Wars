1b22bf3e35ec08b947defe9f551a20690ae65d51
AmteurPotion <1k0j2i4@gmail.com>
2023-03-15 23:53:13 +0900

    Case

--- Assets/Scripts/Game/Entities/Cases/Entity.cs
+++ Assets/Scripts/Game/Entities/Cases/Entity.cs
@@ -3,12 +3,15 @@ using FabricWars.Game.Entities.Functions;
 using FabricWars.Utils.Serialization;
 using UnityEngine;
 
-namespace FabricWars.Game.Entities
+namespace FabricWars.Game.Entities.Cases
 {
     [CreateAssetMenu(fileName = "new Entity", menuName = "Game/Entity/Basic Entity", order = 0)]
     public class Entity : ScriptableObject
     {
+        // stats
         public int maxHealth;
+        
+        // functions
         public List<SerializablePair<float, EntityFunction>> update;
         public List<EntityFunction> onUse;
         public List<EntityFunction> onBreak;

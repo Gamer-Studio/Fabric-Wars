b6b01b487f1acd7144a43271c3dc5391005f38fa
AmteurPotion <1k0j2i4@gmail.com>
2023-03-16 19:32:27 +0900

    원소 작업중

--- Assets/Scripts/Scenes/Board/Attributes/ItemAttributeManager.cs
+++ Assets/Scripts/Scenes/Board/Attributes/ItemAttributeManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using FabricWars.Game.Items;
 using FabricWars.Utils;
+using FabricWars.Utils.KeyBinds;
 using FabricWars.Utils.Serialization;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace FabricWars.Scenes.Board.Attributes
 {
@@ -10,7 +12,11 @@ namespace FabricWars.Scenes.Board.Attributes
     {
         public static ItemAttributeManager instance { get; private set; }
 
-        public List<SerializablePair<ItemAttribute, GaugeInt>> attributeInventory;
+        public SerializableDictionary<ItemAttribute, ItemAttributeSlot> attributeInventory;
+        
+        private ObjectPool<ItemAttributeSlot> _pool;
+        [SerializeField] private GameObject originalSlot;
+        [SerializeField] private Transform slotContainer;
         [SerializeField] private List<ItemAttributeSlot> slots;
 
         private void Awake()
@@ -20,8 +26,34 @@ namespace FabricWars.Scenes.Board.Attributes
                 Destroy(this);
                 return;
             }
+
+            _pool = new ObjectPool<ItemAttributeSlot>(
+                () => Instantiate(originalSlot, slotContainer).GetComponent<ItemAttributeSlot>(),
+                slot =>
+                {
+                    slots.Add(slot);
+                    slot.name = $"AttributeSlot_{slots.Count}";
+                },
+                slot =>
+                {
+                    slots.Remove(slot);
+                },
+                null, false, 1, 20
+            );
+            
+            KeyBindManager.instance
+                .Bind(BindOptions.downOnly, KeyCode.Alpha1)
+                .Then(() =>
+                {
+                    Debug.Log("1");
+                });
             
             instance = this;
         }
+
+        public bool TryGetSlot(ItemAttribute attr, out ItemAttributeSlot slot) =>
+            attributeInventory.TryGetValue(attr, out slot);
+        
+        
     }
 }

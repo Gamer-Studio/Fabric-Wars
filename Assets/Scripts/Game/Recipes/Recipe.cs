e8ee91d2b6107f889c40e5b076b859cbcf90f0d8
AmteurPotion <1k0j2i4@gmail.com>
2023-03-18 16:31:35 +0900

    레시피 업데이트

--- Assets/Scripts/Game/Recipes/Recipe.cs
+++ Assets/Scripts/Game/Recipes/Recipe.cs
@@ -2,7 +2,7 @@
 
 namespace FabricWars.Game.Recipes
 {
-    [CreateAssetMenu(fileName = "new Recipe", menuName = "Game/Item/Blank Recipe", order = 0)]
+    [CreateAssetMenu(fileName = "new Recipe", menuName = "Game/Recipe/Blank Recipe", order = 0)]
     public class Recipe : ScriptableObject
     {
         
---
+++ Assets/Scripts/Game/Recipes/ScopedRecipe.cs
@@ -0,0 +1,16 @@
+﻿using FabricWars.Game.Elements;
+using FabricWars.Utils.Serialization;
+using UnityEngine;
+
+namespace FabricWars.Game.Recipes
+{
+    [CreateAssetMenu(fileName = "new Scoped Recipe", menuName = "Game/Recipe/Entity Recipe", order = 1)]
+    public class ScopedRecipe : Recipe
+    {
+        public GameObject entity;
+        public SerializablePair<Element, int>[] consumes;
+
+        public SerializablePair<Element, int>[] minScope;
+        public SerializablePair<Element, int>[] maxScope;
+    }
+}

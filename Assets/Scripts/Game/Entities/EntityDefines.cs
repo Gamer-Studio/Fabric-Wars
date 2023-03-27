5fb270a385b0043b3ae972b5f924df2d0238740c
AmteurPotion <1k0j2i4@gmail.com>
2023-03-27 23:04:23 +0900

    Fix entity loading

--- Assets/Scripts/Game/Entities/EntityDefines.cs
+++ Assets/Scripts/Game/Entities/EntityDefines.cs
@@ -6,23 +6,17 @@ namespace FabricWars.Game.Entities
 {
     public partial class Entity
     {
-        private static bool _loaded = false;
-        
         public readonly static Dictionary<string, GameObject> allocated = new ();
         
         public static void Load()
         {
-            if(_loaded) return;
-            
             Addressables.LoadAssetsAsync<GameObject>(new AssetLabelReference{labelString = "EntityPrefab"}, entity =>
             {
                 if (allocated.ContainsKey(entity.name)) return;
                 allocated.Add(entity.name, entity);
-            });
+            }).WaitForCompletion();
             
             Debug.Log($"{allocated.Count} entities loaded");
-            
-            _loaded = true;
         }
     }
 }

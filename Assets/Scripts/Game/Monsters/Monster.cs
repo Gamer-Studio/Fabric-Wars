ec9868fbe2e097ed388fcb55364afa913cb43348
AmteurPotion <1k0j2i4@gmail.com>
2023-03-31 16:54:19 +0900

    몬스터 구현중

---
+++ Assets/Scripts/Game/Monsters/Monster.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace FabricWars.Game.Monsters
+{
+    public class Monster : MonoBehaviour
+    {
+        // Builder
+        public const float WaitForCreation = 5;
+        private bool _inBuilding = false;
+        
+        public void StartBuild()
+        {
+            IEnumerator BuildTimer()
+            {
+                yield return new WaitForSeconds(WaitForCreation);
+                if(_inBuilding) Release();
+            }
+            
+            StartCoroutine(BuildTimer());
+            
+            _inBuilding = true;
+        }
+
+        public void Copy(Monster monster)
+        {
+            
+        }
+        
+        public void BuildAndSpawn(Transform parent, Vector3 position, int count = 1)
+        {
+            if(!_inBuilding) return;
+
+            transform.SetParent(parent);
+            transform.position = position;
+            
+            for (var i = 1; i < count; i++)
+            {
+                var mob = MonsterManager.instance.GetMonsterBase();
+                mob.Copy(this);
+                mob.transform.SetParent(parent);
+                mob.transform.position = position;
+            }
+
+            _inBuilding = false;
+        }
+        
+        public void Release()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Monsters/MonsterManager.cs
@@ -0,0 +1,39 @@
+﻿using FabricWars.Utils;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace FabricWars.Game.Monsters
+{
+    public class MonsterManager : ManagerSingleton<MonsterManager>
+    {
+        // pool
+        private ObjectPool<Monster> pool;
+        private Transform _poolContainer;
+        [SerializeField] private GameObject monsterBase;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            pool = new ObjectPool<Monster>(
+                () => Instantiate(monsterBase, Vector3.zero, Quaternion.identity, _poolContainer)
+                    .GetComponent<Monster>(),
+                mob =>
+                {
+                    mob.gameObject.SetActive(true);
+                    mob.StartBuild();
+                },
+                mob =>
+                {
+                    mob.gameObject.transform.SetParent(_poolContainer);
+                    mob.gameObject.SetActive(false);
+                },
+                mob => Destroy(mob.gameObject),
+                true, 10, 10000
+            );
+        }
+        
+        public Monster GetMonsterBase() => pool.Get();
+        public void Release(Monster monster) => pool.Release(monster);
+    }
+}

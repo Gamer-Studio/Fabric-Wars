c3bd4b450b2c241afd93de6465397d94494064de
Sharlotte <aaa9810321@gmail.com>
2023-04-03 16:55:18 +0900

    feat: MaterialExtension `SetConfig` 구현

--- Assets/Scripts/Utils/Extensions/MaterialExtensions.cs
+++ Assets/Scripts/Utils/Extensions/MaterialExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FabricWars.Utils.Serialization;
 using UnityEngine;
 
@@ -29,11 +30,29 @@ namespace FabricWars.Utils.Extensions
         public static void SetBool(this Material material, string name, bool value) =>
             material.SetInt(name, value ? 1 : 0);
 
-        /* TODO
-         * configs 의 object값을 switch로 분리한뒤에 foreach 통해서 int 키 값에 따라 통합적으로 변경
-         */
         public static Material SetConfig(this Material material, params SerializablePair<int, object>[] configs)
         {
+            foreach((int id, object obj) in configs)
+            {
+                Action callback = obj switch
+                {
+                    bool => () => material.SetBool(id, (bool)obj),
+                    ComputeBuffer => () => material.SetBuffer(id, (ComputeBuffer)obj),
+                    Color => () => material.SetColor(id, (Color)obj),
+                    List<Color> => () => material.SetColorArray(id, (List<Color>)obj),
+                    float => () => material.SetFloat(id, (float)obj),
+                    List<float> => () => material.SetFloatArray(id, (List<float>)obj),
+                    int => () => material.SetInteger(id, (int)obj),
+                    Matrix4x4 => () => material.SetMatrix(id, (Matrix4x4)obj),
+                    List<Matrix4x4> => () => material.SetMatrixArray(id, (List<Matrix4x4>)obj),
+                    Texture => () => material.SetTexture(id, (Texture)obj), // SetTextureOffset와 Scale는 어떻게 처리함?
+                    Vector4 => () => material.SetVector(id, (Vector4)obj),
+                    List<Vector4> => () => material.SetVectorArray(id, (List<Vector4>)obj),
+                    _ => throw new NotImplementedException()
+                };
+                callback();
+            }
+
             return material;
         }
     }

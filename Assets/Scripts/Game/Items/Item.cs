2ae2ffefb50fb80ff17d0ddf8b4057cc85d8d19f
Sharlotte <aaa9810321@gmail.com>
2023-04-03 17:40:16 +0900

    feat: content localization (wip)

--- Assets/Scripts/Game/Items/Item.cs
+++ Assets/Scripts/Game/Items/Item.cs
@@ -3,6 +3,7 @@ using FabricWars.Game.Elements;
 using FabricWars.Scenes.Board;
 using FabricWars.Utils.Serialization;
 using UnityEngine;
+using UnityEngine.Localization.Metadata;
 using UnityEngine.Localization.Tables;
 
 namespace FabricWars.Game.Items
@@ -10,7 +11,7 @@ namespace FabricWars.Game.Items
     [CreateAssetMenu(fileName = "new Item", menuName = "Game/Item/Basic Item", order = 0)]
     public partial class Item : ScriptableObject
     {
-        private static readonly LocalizationTable contentTable;
+        [SerializeField] private SerializableDictionary<string, StringTable> contentTable;
 
         public int weight = 0;
         public Sprite sprite;
@@ -20,17 +21,21 @@ namespace FabricWars.Game.Items
         public ItemObject Create(Vector2 position)
         {
             if (!ItemManager.instance) throw new NullReferenceException("ItemManager is not activated");
-
             return ItemManager.instance.Create(this, position);
         }
 
-        public string GetDescription()
+        public string GetLocalizedName()
         {
-            /* TODO
-             * Get item description from localization {Content} table
-             * Key - {item.name}.description
-             */
+            if (contentTable.TryGetValue("ko", out StringTable table))
+                return table.GetEntry($"{name.ToLower()}.name").GetLocalizedString();
             return "";
         }
+
+        public string GetLocalizedDescription()
+        {
+            if(contentTable.TryGetValue("ko", out StringTable table))
+                return table.GetEntry($"{name.ToLower()}.description").GetLocalizedString();
+            return "";
+        } 
     }
 }

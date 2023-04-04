121c79a5cace74701e1d5668009eb352d5bc5617
Sharlotte <aaa9810321@gmail.com>
2023-04-04 14:42:53 +0900

    feat: add timeController

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,27 @@
+﻿using FabricWars.Utils.Singletons;
+using System.Collections;
+using UnityEngine;
+
+namespace FabricWars
+{
+    class TimeController : LazyDDOLSingletonMonoBehaviour<TimeController>
+    {
+        float cachedScale;
+        public bool paused = false;
+
+        public void Pause()
+        {
+            if (paused) return;
+            paused = true;
+            cachedScale = Time.timeScale;
+            Time.timeScale = 0;
+        }
+
+        public void UnPause()
+        {
+            if (!paused) return;
+            paused = false;
+            Time.timeScale = cachedScale;
+        }
+    }
+}

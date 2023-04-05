7d37820a92d345993ae16129cfc051c124856669
AmteurPotion <1k0j2i4@gmail.com>
2023-04-05 14:37:47 +0900

    몬스터 둥지 만드는중

--- Assets/Scripts/Game/Entities/MonsterNest.cs
+++ Assets/Scripts/Game/Entities/MonsterNest.cs
@@ -1,6 +1,8 @@
-﻿using FabricWars.Game.Elements;
+﻿using System;
+using FabricWars.Game.Elements;
 using FabricWars.Utils.Attributes;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Rendering.Universal;
 
 namespace FabricWars.Game.Entities
@@ -9,7 +11,6 @@ namespace FabricWars.Game.Entities
     {
         [SerializeField] private Light2D elementMark;
         [SerializeField, GetSet("element")] private Element _element = Element.None;
-
         public Element element
         {
             get => _element;
@@ -21,6 +22,35 @@ namespace FabricWars.Game.Entities
                     if (elementMark != null) elementMark.lightCookieSprite = element.mark;
                 }
             }
+        }
+
+        public UnityEvent<int> OnLevelChanged;
+        [SerializeField, GetSet("level")] private int _level;
+        public int level
+        {
+            get => _level;
+            set
+            {
+                _level = value;
+                OnLevelChanged.Invoke(value);
+            }
+        }
+
+        public UnityEvent<float> OnResize;
+        [SerializeField, GetSet("size")] private float _size;
+        public float size
+        {
+            get => _size;
+            set
+            {
+                _size = value;
+                OnResize.Invoke(value);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log(collision.gameObject.name);
         }
     }
 }

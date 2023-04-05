65ca89f775dcfec26e25c614c10cfcb3dd309200
Sharlotte <aaa9810321@gmail.com>
2023-04-05 13:01:03 +0900

    feat: add DialogManager

---
+++ Assets/Scripts/UI/DialogManager.cs
@@ -0,0 +1,21 @@
+﻿using FabricWars.Assets.Scripts.UI;
+using FabricWars.Utils.Singletons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class DialogManager : DDOLSingletonMonoBehaviour<DialogManager>
+{
+    [Header("Preset")]
+    [SerializeField] private GameObject baseDialogPref;
+    [SerializeField] private Canvas canvas;
+
+    public void Show(BaseDialogData data)
+    {
+        BaseDialog baseDialog = Instantiate(baseDialogPref, canvas.transform).GetComponent<BaseDialog>();
+        baseDialog.Build(data);
+    }
+}

c0a0ca180a9850b65ecc257958dd658db3c51895
Sharlotte <aaa9810321@gmail.com>
2023-04-05 13:23:20 +0900

    feat: add InputDialog

--- Assets/Scripts/UI/DialogManager.cs
+++ Assets/Scripts/UI/DialogManager.cs
@@ -10,12 +10,18 @@ using UnityEngine;
 class DialogManager : DDOLSingletonMonoBehaviour<DialogManager>
 {
     [Header("Preset")]
-    [SerializeField] private GameObject baseDialogPref;
     [SerializeField] private Canvas canvas;
+    [SerializeField] private GameObject baseDialogPref, inputDialogPref;
 
     public void Show(BaseDialogData data)
     {
         BaseDialog baseDialog = Instantiate(baseDialogPref, canvas.transform).GetComponent<BaseDialog>();
         baseDialog.Build(data);
+    }
+
+    public void ShowInputDialog(InputDialogData data)
+    {
+        InputDialog inputDialog = Instantiate(inputDialogPref, canvas.transform).GetComponent<InputDialog>();
+        inputDialog.Build(data);
     }
 }
---
+++ Assets/Scripts/UI/InputDialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace FabricWars.Assets.Scripts.UI
+{
+    public struct InputDialogData
+    {
+        public string title;
+        public string description;
+        public Action<string> onSubmit;
+    }
+
+    public class InputDialog : Dialog<InputDialogData>
+    {
+        [SerializeField] private TextMeshProUGUI titleTMP, descriptionTMP;
+        public Action<string> onSubmit;
+        private string _currentInputValid = "";
+
+        public override void Build(InputDialogData data)
+        {
+            base.Build(data);
+            titleTMP.SetText(data.title);
+            descriptionTMP.SetText(data.description);
+            onSubmit = data.onSubmit;
+        }
+
+        public void OnInputChange(string value) => _currentInputValid = value;
+        public void OnSubmitButtonClick() => onSubmit?.Invoke(_currentInputValid);
+    }
+}

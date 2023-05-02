5b9a5f384c0bf639ada708d6fb0a07bd92a3ba9e
AmteurPotion <1k0j2i4@gmail.com>
2023-05-03 04:11:55 +0900

    h

--- Assets/Scripts/Scenes/Board/Player.cs
+++ Assets/Scripts/Scenes/Board/Player.cs
@@ -1,22 +1,51 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using FabricWars.Game.Entities;
+using FabricWars.Utils.Extensions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace FabricWars.Scenes.Board
 {
     public class Player : Entity
     {
         public static readonly List<Player> players = new();
-
+        [SerializeField] private Vector2 direction;
+        [SerializeField] private Rigidbody2D body;
+        
         protected override void Awake()
         {
             base.Awake();
+
+            if (body == null) body = GetComponent<Rigidbody2D>();
             
+            EventSystem.current.GetComponent<PlayerInput>().onActionTriggered += context =>
+            {
+                if (context.action.name == "Navigate")
+                {
+                    direction = context.ReadValue<Vector2>();
+                }
+            };
+
             players.Add(this);
+
+            StartCoroutine(Move());
         }
 
         private void OnDestroy()
         {
             players.Remove(this);
+        }
+
+        public float speed = 4;
+        private IEnumerator Move()
+        {
+            while (true)
+            {
+                body.AddForce(direction.Multiply(speed));
+                yield return new WaitForFixedUpdate();
+            }
         }
     }
 }

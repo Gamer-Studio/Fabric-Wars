e4aa42f6007624a269f640a21d4abef29e99b409
AmteurPotion <1k0j2i4@gmail.com>
2023-05-03 15:11:01 +0900

    To Abstract

--- Assets/Scripts/Game/Entities/Functions/EntityFunction.cs
+++ Assets/Scripts/Game/Entities/Functions/EntityFunction.cs
@@ -3,12 +3,8 @@ using UnityEngine;
 
 namespace FabricWars.Game.Entities.Functions
 {
-    [CreateAssetMenu(fileName = "new Entity Function", menuName = "Game/Function/Entity Function", order = 0)]
-    public class EntityFunction : ScriptableObject
+    public abstract class EntityFunction : ScriptableObject
     {
-        public virtual IEnumerator GetFunction()
-        {
-            return null;
-        }
+        public abstract IEnumerator GetFunction();
     }
 }

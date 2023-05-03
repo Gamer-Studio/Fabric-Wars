3c42dde6fa3a2753e9da9eb7c27cd9cf44a3b35a
AmteurPotion <1k0j2i4@gmail.com>
2023-05-03 15:11:09 +0900

    Update GetSetDrawer.cs

--- Assets/Scripts/Editor/Utils/GetSetDrawer.cs
+++ Assets/Scripts/Editor/Utils/GetSetDrawer.cs
@@ -11,42 +11,101 @@ namespace FabricWars.Editor.Utils
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            var attribute = (GetSetAttribute)this.attribute;
+            var attr = (GetSetAttribute)attribute;
+            var isProperty = true;
+            object value = null;
 
             EditorGUI.BeginChangeCheck();
-            EditorGUI.PropertyField(position, property, label);
-
-            if (EditorGUI.EndChangeCheck())
+            switch (property.type)
             {
-                attribute.dirty = true;
+                case "int":
+                {
+                    var i = EditorGUI.IntField(position, label, property.intValue);
+                    if (i != property.intValue)
+                    {
+                        isProperty = false;
+                        value = i;
+                    }
+                    
+                    break;
+                }
+                case "string":
+                {
+                    var s = EditorGUI.TextField(position, label, property.stringValue);
+                    if (!s.Equals(property.stringValue))
+                    {
+                        isProperty = false;
+                        value = s;
+                    }
+
+                    break;
+                }
+
+                case "bool":
+                {
+                    var b = EditorGUI.Toggle(position, label, property.boolValue);
+                    if (b != property.boolValue)
+                    {
+                        isProperty = false;
+                        value = b;
+                    }
+                    
+                    break;
+                }
+
+                case "byte":
+                {
+                    var i = EditorGUI.IntField(position, label, property.intValue);
+                    if (i != property.intValue)
+                    {
+                        isProperty = false;
+                        value = (byte)i;
+                    }
+                    
+                    break;
+                }
+                
+                default:
+                {
+                    Debug.Log(property.type);
+                    EditorGUI.PropertyField(position, property, label);
+                    break;
+                }
             }
-            else if (attribute.dirty)
+            
+            if (EditorGUI.EndChangeCheck() && isProperty)
+            {
+                attr.dirty = true;
+            }
+            else if (attr.dirty || !isProperty)
             {
                 var parent = GetParentObject(property.propertyPath, property.serializedObject.targetObject);
 
                 var type = parent.GetType();
-                var info = type.GetProperty(attribute.name);
+                var info = type.GetProperty(attr.name);
 
                 if (info == null)
-                    Debug.LogError("Invalid property name \"" + attribute.name + "\"");
+                    Debug.LogError("Invalid property name \"" + attr.name + "\"");
                 else
-                    info.SetValue(parent, fieldInfo.GetValue(parent), null);
+                    info.SetValue(parent, isProperty ? fieldInfo.GetValue(parent) : value, null);
 
-                attribute.dirty = false;
+                attr.dirty = false;
             }
         }
 
-        public static object GetParentObject(string path, object obj)
+        private static object GetParentObject(string path, object obj)
         {
-            var fields = path.Split('.');
+            while (true)
+            {
+                var fields = path.Split('.');
 
-            if (fields.Length == 1)
-                return obj;
+                if (fields.Length == 1) return obj;
 
-            FieldInfo info = obj.GetType().GetField(fields[0], BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            obj = info.GetValue(obj);
+                var info = obj.GetType().GetField(fields[0], BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                obj = info.GetValue(obj);
 
-            return GetParentObject(string.Join(".", fields, 1, fields.Length - 1), obj);
+                path = string.Join(".", fields, 1, fields.Length - 1);
+            }
         }
     }
 }

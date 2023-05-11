1e51689ee80b97fbaf2a3cee1403ab4b8bfbfa14
AmteurPotion <1k0j2i4@gmail.com>
2023-05-12 01:14:42 +0900

    청크 기반 월드 구현중

---
+++ Assets/Scripts/Worlds/Chunk.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+namespace FabricWars.Worlds
+{
+    [Serializable]
+    public class Chunk
+    {
+#if UNITY_EDITOR
+        public int s_size;
+#endif
+        public readonly int size = 20;
+        public Vector3Int position;
+        private Tilemap _tilemap;
+        private TileBase[] tiles;
+
+        public Chunk(int size, Vector3Int position, Tilemap tilemap, TileBase[] tiles)
+        {
+#if UNITY_EDITOR
+            s_size = size;
+#endif
+            
+            this.size = size;
+            this.position = position;
+            this._tilemap = tilemap;
+            this.tiles = tiles;
+            Generate();
+        }
+
+        private void Generate()
+        {
+            for (var x = 0; x < size; x++)
+            {
+                for (var y = 0; y < size; y++)
+                {
+                    var tilePosition = new Vector3Int(position.x * size + x, position.y * size + y, 0);
+                    _tilemap.SetTile(tilePosition, GetTileAt(tilePosition));
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            var bounds = new BoundsInt(position * size, new Vector3Int(size, size, 1));
+            _tilemap.SetTilesBlock(bounds, new TileBase[size * size]);
+        }
+
+        private TileBase GetTileAt(Vector3Int position)
+        {
+            // 여기서 원하는 타일맵 생성 알고리즘을 구현하세요.
+            // 예를 들어, 무작위 타일을 선택할 수 있습니다.
+            var randomIndex = Random.Range(0, tiles.Length);
+            return tiles[randomIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/Worlds/World.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using FabricWars.Utils.Serialization;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace FabricWars.Worlds
+{
+    public class World : MonoBehaviour
+    {
+        public const int ChunkSize = 20;
+        public int viewDistance;
+        public Tilemap tilemap;
+        public TileBase[] tiles;
+
+        [SerializeField] private Vector3Int previousPlayerChunk;
+        [SerializeField] private Transform playerTransform;
+        [SerializeField] private SerializableDictionary<Vector3Int, Chunk> loadedChunks = new ();
+
+        private void Start()
+        {
+            playerTransform = Camera.main.transform;
+            previousPlayerChunk = WorldToChunkPosition(playerTransform.position);
+            LoadChunks();
+        }
+
+        private void Update()
+        {
+            var currentChunk = WorldToChunkPosition(playerTransform.position);
+            if (currentChunk != previousPlayerChunk)
+            {
+                previousPlayerChunk = currentChunk;
+                LoadChunks();
+            }
+        }
+
+        private static Vector3Int WorldToChunkPosition(Vector3 worldPos)
+        {
+            return new Vector3Int(Mathf.FloorToInt(worldPos.x / ChunkSize), Mathf.FloorToInt(worldPos.y / ChunkSize),
+                0);
+        }
+
+        private void LoadChunks()
+        {
+            var newChunks = new HashSet<Vector3Int>();
+
+            for (var x = -viewDistance; x <= viewDistance; x++)
+            {
+                for (var y = -viewDistance; y <= viewDistance; y++)
+                {
+                    var chunkPos = new Vector3Int(previousPlayerChunk.x + x, previousPlayerChunk.y + y, 0);
+                    if (!loadedChunks.ContainsKey(chunkPos))
+                    {
+                        var newChunk = new Chunk(ChunkSize, chunkPos, tilemap, tiles);
+                        loadedChunks.Add(chunkPos, newChunk);
+                    }
+
+                    newChunks.Add(chunkPos);
+                }
+            }
+
+            var chunksToRemove = new List<Vector3Int>();
+
+            foreach (var (position, chunk) in loadedChunks)
+            {
+                if (!newChunks.Contains(position))
+                {
+                    chunk.Clear();
+                    chunksToRemove.Add(position);
+                }
+            }
+
+            foreach (var removeChunkPos in chunksToRemove)
+            {
+                loadedChunks.Remove(removeChunkPos);
+            }
+        }
+    }
+}

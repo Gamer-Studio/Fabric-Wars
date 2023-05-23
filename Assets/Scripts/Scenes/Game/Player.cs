be5ee0c2b161d27eb85e21dc90b15641e3c834f3
AmteurPotion <1k0j2i4@gmail.com>
2023-05-23 18:03:40 +0900

    멀티플레이어를 위한 인풋시스템 분리

--- Assets/Scripts/Scenes/Game/Player.cs
+++ Assets/Scripts/Scenes/Game/Player.cs
@@ -1,36 +1,28 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using FabricWars.Game;
 using FabricWars.Game.Entities;
 using FabricWars.Game.Items;
 using FabricWars.Utils.Extensions;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 
 namespace FabricWars.Scenes.Game
 {
     public class Player : Entity
     {
         public static readonly List<Player> players = new();
+        
         public Inventory inventory;
         [SerializeField] private Vector2 direction;
+        [SerializeField] private bool isMoving = false;
         [SerializeField] private Rigidbody2D body;
+        [SerializeField] private LineRenderer line;
         
         protected override void Awake()
         {
             base.Awake();
 
             if (body == null) body = GetComponent<Rigidbody2D>();
-            
-            EventSystem.current.GetComponent<PlayerInput>().onActionTriggered += context =>
-            {
-                if (context.action.name == "Navigate")
-                {
-                    direction = context.ReadValue<Vector2>();
-                }
-            };
 
             players.Add(this);
 
@@ -43,12 +35,23 @@ namespace FabricWars.Scenes.Game
             players.Remove(this);
         }
 
+        internal void NavigateTrigger(Vector2 navDir)
+        {
+            if (navDir == Vector2.zero) isMoving = false;
+            else
+            {
+                direction = navDir;
+                isMoving = true;
+                if(line != null) line.SetPosition(1, navDir * 2);
+            }
+        }
+
         public float speed = 4;
         private IEnumerator Move()
         {
             while (true)
             {
-                body.AddForce(direction.Multiply(speed));
+                if(isMoving) body.AddForce(direction * speed);
                 yield return new WaitForFixedUpdate();
             }
         }
@@ -63,7 +66,7 @@ namespace FabricWars.Scenes.Game
 
         public void Interaction_1()
         {
-            
+            Debug.Log(direction);
         }
 
         public void Interaction_2()
--- Assets/Scripts/Scenes/Game/PlayerManager.cs
+++ Assets/Scripts/Scenes/Game/PlayerManager.cs
@@ -1,27 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.ResourceManagement.Util;
 
 namespace FabricWars.Scenes.Game
 {
     public sealed class PlayerManager : ComponentSingleton<PlayerManager>
     {
+        public PlayerInput input;
         public Player currentPlayer;
-        
-        [Header("Viewer Panels")] 
-        public InventoryViewer inventoryViewer;
 
-        [Header("KeyBinds"), SerializeField] 
-        public KeyCode interaction1, interaction2;
+        [Header("Viewer Panels")] public InventoryViewer inventoryViewer;
+
+        [Header("KeyBinds"), SerializeField] public KeyCode interaction1, interaction2;
 
         private void Awake()
         {
             Settings.keyMappings.TryGetValue("playerManager.interaction1", out interaction1);
             Settings.keyMappings.TryGetValue("playerManager.interaction2", out interaction2);
+
+            if (input == null) input = PlayerInput.all[0];
+
+            input.onActionTriggered += context =>
+            {
+                if (context.action.name == "Navigate" && currentPlayer != null)
+                    currentPlayer.NavigateTrigger(context.ReadValue<Vector2>());
+            };
         }
 
         private void Update()
         {
-
             if (currentPlayer != null)
             {
                 if (Input.GetKeyDown(interaction1)) currentPlayer.Interaction_1();

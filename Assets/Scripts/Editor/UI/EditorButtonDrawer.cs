8035df1f66227ea5b1857048b0cc709791b92238
AmteurPotion <1k0j2i4@gmail.com>
2023-05-23 18:01:17 +0900

    에디터 버튼 추가
    Co-Authored-By: Sharlotte  <aaa9810321@gmail.com>

---
+++ Assets/Scripts/Editor/UI/EditorButtonDrawer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using FabricWars.Graphics.EditorUI;
+using UnityEditor;
+using UnityEngine;
+
+namespace FabricWars.Editor.UI
+{
+    [CustomEditor(typeof(Object), true)]
+    public class EditorButtonDrawer : UnityEditor.Editor
+    {
+        private readonly List<(MethodInfo, ButtonAttribute)> _buttons = new();
+
+        private void OnEnable()
+        {
+            var methods = target.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Static |
+                                                      BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (var method in methods)
+            {
+                var attribute = method.GetCustomAttribute<ButtonAttribute>();
+                
+                if (attribute != null) _buttons.Add((method, attribute));
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            foreach (var (method, attribute) in _buttons)
+                if (GUILayout.Button(attribute.name))
+                    method.Invoke(target, null);
+        }
+    }
+}
---
+++ Assets/Scripts/Graphics/EditorUI/ButtonAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FabricWars.Graphics.EditorUI
+{
+    /// <summary>
+    /// Inspector button metadata
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ButtonAttribute : Attribute
+    {
+        public readonly string name;
+
+        public ButtonAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+}

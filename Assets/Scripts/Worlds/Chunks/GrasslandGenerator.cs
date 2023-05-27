9a0ff80c3ed7344432afe0dee90e6e4f81587ff4
AmteurPotion <1k0j2i4@gmail.com>
2023-05-27 14:18:25 +0900

    Update GrasslandGenerator.cs

--- Assets/Scripts/Worlds/Chunks/GrasslandGenerator.cs
+++ Assets/Scripts/Worlds/Chunks/GrasslandGenerator.cs
@@ -1,14 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using FabricWars.Worlds.Tiles;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace FabricWars.Worlds.Chunks
 {
     [CreateAssetMenu(fileName = "new Grassland Generator", menuName = "World/Chunk/Grassland")]
     public class GrasslandGenerator : ChunkGenerator
     {
+        public WorldTile baseTile;
         public override Chunk Generate(World world, Vector3Int position)
         {
             var (chunk, layers, chunkSize) = base.Generate(world, position);
-
+            
+            layers[1].SetTilesBlock(new BoundsInt(position * chunkSize, new Vector3Int(chunkSize, chunkSize, 1)),
+                Enumerable.Repeat<TileBase>(baseTile, chunkSize * chunkSize).ToArray());
+            
+            
             return chunk;
         }
     }
